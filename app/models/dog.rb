bced9020d86433f68997c10ba87f6b88259cc47f
franco <franconiderhaus@gmail.com>
2023-11-02 23:40:12 -0300

    perros muy avanzados

---
+++ app/models/dog.rb
@@ -0,0 +1,33 @@
+class Dog < ApplicationRecord
+    belongs_to :user, default: -> {Current.user}
+
+    validates :nombre, presence: true,
+  format: {
+    with: /\A[a-z-A-Z]+\z/,
+    message: "solo se permiten letras en el nombre"
+  }
+
+  validates :raza, presence: true
+
+  validates :sexo, presence: true,
+  format: {
+    with: /\A[a-z-A-Z]+\z/,
+    message: :invalid
+  }
+
+  validates :color, presence: true,
+  format: {
+    with: /\A[a-z-A-Z]+\z/,
+    message: "solo se permiten letras en el color"
+  }
+
+
+  validates :peso, presence: true
+  
+  validates :edad, presence: true,
+  format: {
+    with: /\A[0-9]+\z/,
+    message: "debe tener solo números"
+  }
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,45 @@
+class User < ApplicationRecord
+  has_secure_password
+  
+  has_many :turno, dependent: :destroy
+  has_one :profile, dependent: :destroy
+  has_many :dog , dependent: :destroy
+  after_create :set_profile
+
+
+  validates :email, presence: true, uniqueness: true,
+  format: { with: /\A([\w+-].?)+@[a-z\d-]+(.[a-z]+)*.[a-z]+\z/i,
+    message: "formato del mail invalido"
+  }
+
+  validates :dni, 
+  presence: true,
+  uniqueness: true,
+  format: {
+    with: /\A[0-9]+\z/,
+    message: "debe tener solo números"
+  }
+
+  validates :password, presence: true
+  validates :nombre, presence: true,
+  format: {
+    with: /\A[a-z-A-Z]+\z/,
+    message: "solo se permiten letras en el nombre"
+  }
+  validates :apellido, presence: true,
+  format: {
+      with: /\A[a-z-A-Z]+\z/,
+      message: "solo se permiten letras en el apellido"
+    }
+  validates :rol, presence: true
+
+  before_save :downcase_attributes
+
+  private
+  def set_profile
+    @profile = Profile.new
+  end
+  def downcase_attributes
+      self.email = email.downcase
+  end
+end
